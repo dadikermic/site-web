8ce25ac7d820a1b52a52feb0f386bae54eb43d2d
dadikermic <38631919+dadikermic@users.noreply.github.com>
2018-12-07 18:10:34 +0100

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="layout/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="layout/css/bootstrap-rtl.css">
+  <link rel="stylesheet" type="text/css" href="layout/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="layout/css/style.css">
+  <SCRIPT LANGUAGE="Javascript" SRC= "layout/js/script3.js" > </SCRIPT>
+  <script src="layout/js/jquery.js"></script>
+  <script src="layout/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+ <div class="navbar navbar-inverse newnavv">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="admin.php"> نقطة التطوير</a>
+
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+      <section class="menu-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="navbar-collapse collapse ">
+                            <ul id="menu-top" class="nav navbar-nav navbar-right">
+
+                                <li class="dropfont">
+                                    <a  href="admin.php">
+                                     <i class="fa fa-user" > الصفحة الشخصية</i>
+                                    </a>
+                           
+                                </li>
+                                <li class="dropdown dropfont">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                            <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"> الرسائل</i>
+                                        </a>
+                                        <ul class="dropdown-menu dropdown-messages">
+                                            <li>
+                                                <a href="#">
+                                                    <div>
+                                                        <strong>كرميش الدراجي</strong>
+                                                        <span class="pull-left text-muted">
+                                                            <em>4 دقائق مضت</em>
+                                                        </span>
+                                                    </div>
+                                                    <div>ةبزنتلارةرةىرةىرىةرةىرةىرةىرةىر...</div>
+                                                </a>
+                                            </li>
+                                            <li class="divider"></li>
+                                            <li>
+                                                <a href="#">
+                                                    <div>
+                                                        <strong>كرميش الدراجي</strong>
+                                                        <span class="pull-left text-muted">
+                                                            <em>4 دقائق مضت</em>
+                                                        </span>
+                                                    </div>
+                                                    <div>ةبزنتلزنتزمناتظمنت...</div>
+                                                </a>
+                                            </li>
+                                            <li class="divider"></li>
+                                            <li>
+                                                <a href="#">
+                                                    <div>
+                                                        <strong>كرميش الدراجي</strong>
+                                                        <span class="pull-left text-muted">
+                                                            <em>4 دقائق مضت</em>
+                                                        </span>
+                                                    </div>
+                                                    <div>ةبزنتلزنتزمناتظمنت...</div>
+                                                </a>
+                                            </li>
+                                            <li class="divider"></li>
+                                            <li>
+                                                <a class="text-center" href="#">
+                                                     <i class="fa fa-angle-left"></i>
+                                                    <strong>مشاهدة كل الرسائل</strong>
+                                                    
+                                                </a>
+                                            </li>
+                                        </ul>
+                                        <!-- /.dropdown-messages -->
+                                </li>
+                                <!-- /.dropdown -->
+                                <li class="dropdown dropfont">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                        <i class="fa fa-tasks fa-fw"></i> <i class="fa fa-caret-down"> التبليغات</i>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-tasks">
+                                        <li>
+                                            <a href="#">
+                                                <div>
+                                                        <strong>كرميش الدراجي</strong>
+                                                        <div class="text-muted">قام بالتبليغ عن منشور مسيئ</div>
+                                                    
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a href="#">
+                                                <div>
+                                                        <strong>كرميش الدراجي</strong>
+                                                        <div class="text-muted">قام بالتبليغ عن منشور مسيئ</div>
+                                                    
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a href="#">
+                                                <div>
+                                                        <strong>كرميش الدراجي</strong>
+                                                        <div class="text-muted">قام بالتبليغ عن منشور مسيئ</div>
+                                                    
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a href="#">
+                                                <div>
+                                                        <strong>كرميش الدراجي</strong>
+                                                        <div class="text-muted">قام بالتبليغ عن منشور مسيئ</div>
+                                                    
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a class="text-center" href="#">
+                                               <i class="fa fa-angle-left"></i>
+                                                <strong>مشاهدة كل التبليغات</strong>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <!-- /.dropdown-tasks -->
+                                </li>
+                                <!-- /.dropdown -->
+                               
+                <li class="dropfont"><a href="index1.php?logout='1'" ><i class="fa fa-power-off fa-fw"></i> خروج</a>
+                                </li>
+                   
+                            </ul>
+             
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </section>
+
+          <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 page-head-line">
+                    <h4 >الرئيسية</h4>
+
+                </div>
+            </div>
+         
+            <div class="row">
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                        <a href="#">
+                            <div class="dashboard-div-wrapper ">
+                                <i  class="fa fa-users dashboard-div-icon" ></i>
+                                
+                                 <h5 class="dashboard-div-wrapperh5">المستخدمون</h5>
+                            </div>
+                        </a>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                        <a href="#">
+                            <div class="dashboard-div-wrapper ">
+                                <i  class="fa fa-users dashboard-div-icon" ></i>
+                                
+                                 <h5 class="dashboard-div-wrapperh5">المستخدمون</h5>
+                            </div>
+                        </a>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                        <a href="#">
+                            <div class="dashboard-div-wrapper ">
+                                <i  class="fa fa-users dashboard-div-icon" ></i>
+                                
+                                 <h5 class="dashboard-div-wrapperh5">المستخدمون</h5>
+                            </div>
+                        </a>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                        <a href="#">
+                            <div class="dashboard-div-wrapper ">
+                                <i  class="fa fa-users dashboard-div-icon" ></i>
+                                
+                                 <h5 class="dashboard-div-wrapperh5">المستخدمون</h5>
+                            </div>
+                        </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
